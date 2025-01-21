c0e999ae7dd2b44d7aece9ba3414a96544f45693
Christopher Arndt <chris@chrisarndt.de>
2025-01-21 20:09:36 +0100

    fix: also list simple fomrmats
    Also do format checks using 48k samplre rate and 1 and 2 channels
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- examples/list_formats.nim
+++ examples/list_formats.nim
@@ -6,19 +6,26 @@ import sndfile
 var
   sfinfo: SFInfo
   fmtinfo: SFFormatInfo
-  major_count, subtype_count: cint
+  simpleCount, majorCount, subtypeCount: cint
 
 echo &"Version: {versionString()}\n"
 
-command(nil, GET_FORMAT_MAJOR_COUNT, major_count.addr, sizeof(cint).cint)
-command(nil, GET_FORMAT_SUBTYPE_COUNT, subtype_count.addr, sizeof(cint).cint)
+command(nil, GET_SIMPLE_FORMAT_COUNT, simpleCount.addr, sizeof(cint).cint)
+command(nil, GET_FORMAT_MAJOR_COUNT, majorCount.addr, sizeof(cint).cint)
+command(nil, GET_FORMAT_SUBTYPE_COUNT, subtypeCount.addr, sizeof(cint).cint)
 
-sfinfo.channels = 1
+
+for format in  0..<simpleCount:
+  fmtinfo.format = format.cint
+  command(nil, GET_SIMPLE_FORMAT, fmtinfo.addr, sizeof(fmtinfo).cint)
+  echo &"{fmtinfo.name} (extension: '.{fmtinfo.extension}')"
+
+sfinfo.samplerate = 48000
 
 for major_format in  0..<major_count:
   fmtinfo.format = major_format.cint
   command(nil, GET_FORMAT_MAJOR, fmtinfo.addr, sizeof(fmtinfo).cint)
-  echo &"{fmtinfo.name} (extension \".{fmtinfo.extension}\")"
+  echo &"{fmtinfo.name} (extension '.{fmtinfo.extension}')"
 
   var format = fmtinfo.format
 
@@ -28,9 +35,19 @@ for major_format in  0..<major_count:
 
     format = (format and SFFormatMask.TYPEMASK.cint) or fmtinfo.format
     sfinfo.format = format
+    var valid = false
+    sfinfo.channels = 1
 
     if formatCheck(sfinfo.addr) == TRUE:
-      if not fmtinfo.extension.isNil:
-        echo &"   {fmtinfo.name} (extension \".{fmtinfo.extension}\")"
-      else:
+      valid = true
+
+    sfinfo.channels = 2
+
+    if formatCheck(sfinfo.addr) == TRUE:
+      valid = true
+
+    if valid:
+      if fmtinfo.extension.isNil:
         echo &"   {fmtinfo.name}"
+      else:
+        echo &"   {fmtinfo.name} (extension '.{fmtinfo.extension}')"
